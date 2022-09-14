f104dbc09f1b3115d61f7e82ea40eb3c0ec77aaf
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-09-14 15:21:35 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -20,15 +20,19 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "FrameworkA",
-            path: "./Sources/FrameworkA.xcframework"),
+            url: "https://nexus6.kydom.net/repository/XCFrameworks/FrameworkA/FrameworkA.xcframework.zip",
+            checksum: "d72e8cd9ee2960d5ebb928f6d8eb6753f43fdb2cf9491761d872f5942745c568"),
         .binaryTarget(
             name: "FrameworkB",
-            path: "./Sources/FrameworkB.xcframework"),
+            url: "https://nexus6.kydom.net/repository/XCFrameworks/FrameworkB/FrameworkB.xcframework.zip",
+            checksum: "881042155850c0b753b93c62709f9cc360c606b403b4138017023bbb1605c976"),
         .binaryTarget(
             name: "FrameworkC",
-            path: "./Sources/FrameworkC.xcframework"),
+            url: "https://nexus6.kydom.net/repository/XCFrameworks/FrameworkC/FrameworkC.xcframework.zip",
+            checksum: "ad8402548b59b3a5b1b396ce73bad93d56f1b248a4f1432dce8b526a09dc5f66"),
         .binaryTarget(
             name: "FrameworkD",
-            path: "./Sources/FrameworkD.xcframework"),
+            url: "https://nexus6.kydom.net/repository/XCFrameworks/FrameworkD/FrameworkD.xcframework.zip",
+            checksum: "2b95c789070a2e495c285269a0257c48230565e4c1633d080808a0d70ae8a19a"),
     ]
 )

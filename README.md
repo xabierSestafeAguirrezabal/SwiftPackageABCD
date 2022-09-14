6f5875f0ee5866fc70733eef43927407344900fb
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-09-14 13:45:47 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftPackageABCD
+
+A description of this package.
---
+++ Sources/FrameworkD.xcframework/ios-arm64/FrameworkD.framework/Headers/FrameworkD.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkD.h
+//  FrameworkD
+//
+//  Created by Jose F Gómez Arbaizar on 14/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkD.
+FOUNDATION_EXPORT double FrameworkDVersionNumber;
+
+//! Project version string for FrameworkD.
+FOUNDATION_EXPORT const unsigned char FrameworkDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkD/PublicHeader.h>
+
+
